73c6d468afe33b784e1a55c77c19ecf0bd2f2c61
mangeleso <michel.angeles@gmail.com>
2017-07-21 03:57:50 +0200

    Changed Intents and Entities

--- chatbot.py
+++ chatbot.py
@@ -22,6 +22,14 @@ it just pick a response from a fixed set. The user is able to type anything he i
 the Assistant will display the brands and later the products and price from the input of the user.
 '''
 
+class request:
+	def __init__(self, text, intent, entities):
+		self.text = text
+		self.intent = intent
+		self.entities = entities
+
+
+
 
 def preprocess_senteces( raw_sentences):
 	'''
@@ -29,9 +37,8 @@ def preprocess_senteces( raw_sentences):
 	The input is a string (request from training set)
 	The output is a string (preprocessed request)
 	'''
-
 	#Remove non-letters
-	letters_only = re.sub("[^a-zA-Z0-9]", " ", raw_sentences) 
+	letters_only = re.sub("[^a-zA-Z0-9+]", " ", raw_sentences) 
 
 	#Convert to lower_case
 	words = letters_only.lower().split()
@@ -44,6 +51,51 @@ def preprocess_senteces( raw_sentences):
 
 	#Join the words back into one string 
 	return(" ".join(meaningful_words))
+
+
+def more_data():
+	INTENTS = ['ShowPriceByProduct', 'ShowSimilarProducts']
+
+	phones = ['iphone', 'iphone apple','iphone 7', 'iphone plus', 'iphone 7 plus', 'apple iphone', 'iphone 128gb', 'iphone 32gb', 'iphone plus 128gb',  
+			'samsung', 'galaxy', 'smartphone', 'samsung galaxy', 'galaxy s8', 'galaxy 64gb', 'galaxy s8+ 64gb', 's8', 's8+']
+
+	drones = ['parrot', 'drone', 'bebop', 'bebop 2', 'parrot 2']
+
+	gaming = ['vive', 'htc', 'vr', 'virtual reality', 'glasses', 'oculus', 'rift', 'oculus rift']
+
+	computing = ['mac', 'macbook', 'macbook air', 'mac pro', 'macbook pro', 'macbook 8gb', 'macbook 516gb', 'macbook 512gb', 'macbook air 11', 'macbook air 13', 'macbook pro 13', 'macbook 4gb'
+				'macbook 500gb', 'lenovo', 'yoga', 'surface', 'convertible surface', 'convertible yoga', 'microsoft']
+
+	wearables = ['watch', 'watch 38', 'watch 42', 'watch 42mm', 'watch 38mm', 'apple watch', 'watch apple', 'suunto', 'watch ambit', 'ambit', 'watch v800', 
+				'v800', 'polar', 'asus', 'watch asus', 'asus watch']
+
+	smart = ['amazon alexa', 'alexa', 'alexa dot', 'alexa echo', 'tchibo', 'tchibo qbo', 'qbo', 'qbo milk', 'samsung robot', 'robot vacuum', 'vacuum', 
+			'vacuum cleaner', 'powerbot', 'VR20J9020UR', 'VR20J9259U']
+
+	ENTITIES = { 'phones':phones, 'drones':drones, 'gaming':gaming, 'computing':computing, 'wearables':wearables, 'smart':smart}
+
+	
+	all_entities = phones + drones + gaming + computing + wearables + smart
+
+	show_price = ['show me price', 'how much', 'cost of', 'tell me the price', 'give me the price', 'how much money']
+	show_products = ['show me', 'I want a', 'I need a', 'I wish a', 'I am looking for a', 'I look for a', 'show me a', 'I am interested','tell me about']
+
+	data_show_price = []
+	for ent in all_entities:
+		for s in show_price:
+			data_show_price.append(s + " " + ent)
+	labels_0 = [0] * len(data_show_price)
+
+	data_show_similar_products = []
+	for ent in all_entities:
+		for s in show_products:
+			data_show_similar_products.append(s + " " + ent)
+
+	#Append all entities in the list 
+	data_show_similar_products = data_show_similar_products + all_entities
+	labels_1 = [1] * len(data_show_similar_products) 
+
+	return data_show_price + data_show_similar_products, labels_0 + labels_1
 
 
 
@@ -71,6 +123,79 @@ def create_data():
 				sentences.append(desire + " " + product)
 		labels = labels + [idx] * (len(cat) * len(WORDS_DESIRE))
 	return sentences, labels 
+
+def find_intent(clf, input_sentence, transformer):
+	'''
+	Funtion that maps a input sentence from the user to the 
+	desire category from {Phones: 0, Drones: 1, Gaming:2, Computing: 3, Wearables:4 , Smarthomes:5}
+	It returns the category number 
+	'''
+	#Preprocess the input string
+	clean_sentence = preprocess_senteces(input_sentence)
+
+	#Create the features with the transformer
+	features = transformer.transform([clean_sentence])
+
+	#If the prediction is not so sure from the user input return -1
+	proba = clf.predict_proba(features)[0]
+
+	print(proba)
+	proba = np.array(proba)
+
+	if (proba > 0.9).sum() > 0:
+		return clf.predict(features)[0]
+
+	return -1
+
+
+
+def find_category(clf, input_sentence, transformer):
+	'''
+	Funtion that maps a input sentence from the user to the 
+	desire category from {Phones: 0, Drones: 1, Gaming:2, Computing: 3, Wearables:4 , Smarthomes:5}
+	It returns the category number 
+	'''
+	#Preprocess the input string
+	clean_sentence = preprocess_senteces(input_sentence)
+
+	#Create the features with the transformer
+	features = transformer.transform([clean_sentence])
+
+	#If the prediction is not so sure from the user input return -1
+	proba = clf.predict_proba(features)[0]
+
+	print(proba)
+	proba = np.array(proba)
+
+	if (proba > 0.9).sum() > 0:
+		return clf.predict(features)[0]
+
+	return -1
+
+
+def find_entities(sentence):
+	entities = []
+	clean_s = preprocess_senteces(sentence)
+
+	# function to test if something is a noun
+	is_noun = lambda pos: pos[:2] == 'NN'
+
+	# do the nlp stuff
+	tokenized = nltk.word_tokenize(clean_s)
+	nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)] 
+
+	return nouns
+
+
+
+
+def do_intent(intent, entities, df):
+	if intent == 1: ##Show products similar to entities
+		print("opp")
+
+
+
+
 
 
 def find_category(clf, input_sentence, transformer):
@@ -152,6 +277,8 @@ def display_subscription(index, products, df):
 		print("The price for " + products[index] + " is: " + str(price) + " for a month.")
 
 
+
+
 if __name__ == '__main__':
 	word_lemm = WordNetLemmatizer()
 	
@@ -161,7 +288,9 @@ if __name__ == '__main__':
 
 
 	#Create some data 
-	sentences, labels = create_data()
+	#sentences, labels = create_data()
+	sentences, labels = more_data()
+
 
 	clean_sentences = []
 
@@ -188,11 +317,11 @@ if __name__ == '__main__':
 	train_tfidf = train_tfidf.toarray()
 
 	#Train a classifier
-	forest = RandomForestClassifier(n_estimators = 40)
+	forest = RandomForestClassifier(n_estimators=50)
 	forest = forest.fit(train_data_features, labels)
 
 	#Train a randomforest using td-idf features
-	forest2 = RandomForestClassifier(n_estimators=40)
+	forest2 = RandomForestClassifier(n_estimators=50)
 	forest2 = forest2.fit(train_tfidf, labels)
 
 	#Train a nearest neighbor classifier
@@ -201,7 +330,8 @@ if __name__ == '__main__':
 
 
 	#Some test sentences
-	test_sentences = ['I want to know more about an iphone', 'I am interested in a s8', 'show me price for alexa echo', 'I look for shoes']
+	test_sentences = ['I want to know more about an iphone', 'I am interested in a s8', 'show me price for alexa echo', 'I look for shoes', 'apple iphone']
+
 	clean_test = [preprocess_senteces(x) for x in test_sentences]
 	test_features = vectorizer.transform(clean_test)
 	test_tfidf = vectorizer_tfidf.transform(clean_test)
@@ -219,31 +349,61 @@ if __name__ == '__main__':
 	#plt.show()
 
 
+
+
+	requests = []
+
+
 	cats = pd.unique(df['Category'])
 	no_cats = len(cats)
-	INITIAL_GREETING = "Welcome at Grover!!, I am your personal assistant"
+	INITIAL_GREETING = "Bot: Welcome at Grover!!, I am your personal assistant"
 
 	print(INITIAL_GREETING)
 
 	try: 
 		while True:
-			user_input = input('What are you looking for?\n')
+			print('Bot: What are you looking for?')
+			user_input = input('User: ')
+			intent = find_intent(forest, user_input, vectorizer)
+			entities = find_entities(user_input)
+
+
+			requests.append(request(user_input, intent, entities)) 
+
+			if intent < 0:
+				print("Bot: I am affraid we dont have what are you looking for..:/, please try again.")
+			
+			do_intent(intent, entities, df)
+
+
+			
+
+	except KeyboardInterrupt:
+		print('Have a nice day!')
+
+	'''
+
+	try: 
+		while True:
+			print('Bot: What are you looking for?')
+			user_input = input('User: ')
 			cat = find_category(forest, user_input, vectorizer)
 			
 			if cat < 0:
-				print("I am affraid we dont have what are you looking for..:/, please try again.")
+				print("Bot: I am affraid we dont have what are you looking for..:/, please try again.")
 
 			else:#Print the brands from the data
 
 				brands_interest = display_brands(cat, df)
 
-				#Ask for the brand of interest 
-				brand = preprocess_senteces(input("From which brand you want to see products?"))
+				#Ask for the brand of interest
+				print("Bot: From which brand you want to see products?")
+				brand = preprocess_senteces(input("User: "))
 				if is_a_brand(brands_interest, brand):
 					#Show products from the brand
 					products = display_products(brand, df, cat)
 
-					product_user = input("Please type the id of the product :).")
+					product_user = input("Bot: Please type the id of the product :).")
 
 					#Display the plab subscription
 					display_subscription(int(product_user), products, df)
@@ -252,6 +412,7 @@ if __name__ == '__main__':
 	except KeyboardInterrupt:
 		print('Have a nice day!')
 
+	'''
 
 
 
