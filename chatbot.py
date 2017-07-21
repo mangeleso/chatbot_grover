406e972420deb924abda792d3f5ec31811d0d492
mangeleso <michel.angeles@gmail.com>
2017-07-21 15:06:46 +0200

    Fixed intents

--- chatbot.py
+++ chatbot.py
@@ -28,9 +28,6 @@ class request:
 		self.intent = intent
 		self.entities = entities
 
-
-
-
 def preprocess_senteces( raw_sentences):
 	'''
 	Function that converts a raw request to a string of words
@@ -54,31 +51,41 @@ def preprocess_senteces( raw_sentences):
 
 
 def more_data():
-	INTENTS = ['ShowPriceByProduct', 'ShowSimilarProducts']
+	'''
+	This is an example to create some data in order to train the machine learning model to classify the intents 
+	'''
+
+	INTENTS = ['ShowPriceByProduct', 'ShowSimilarProducts', 'ShowProductsFromBrand']
 
 	phones = ['iphone', 'iphone apple','iphone 7', 'iphone plus', 'iphone 7 plus', 'apple iphone', 'iphone 128gb', 'iphone 32gb', 'iphone plus 128gb',  
-			'samsung', 'galaxy', 'smartphone', 'samsung galaxy', 'galaxy s8', 'galaxy 64gb', 'galaxy s8+ 64gb', 's8', 's8+']
+			 'galaxy', 'smartphone', 'samsung galaxy', 'galaxy s8', 'galaxy 64gb', 'galaxy s8+ 64gb', 's8', 's8+']
 
-	drones = ['parrot', 'drone', 'bebop', 'bebop 2', 'parrot 2']
+	drones = ['drone', 'bebop', 'bebop 2']
 
-	gaming = ['vive', 'htc', 'vr', 'virtual reality', 'glasses', 'oculus', 'rift', 'oculus rift']
+	gaming = ['vive', 'htc', 'vr', 'virtual reality', 'glasses', 'rift', 'oculus rift']
 
 	computing = ['mac', 'macbook', 'macbook air', 'mac pro', 'macbook pro', 'macbook 8gb', 'macbook 516gb', 'macbook 512gb', 'macbook air 11', 'macbook air 13', 'macbook pro 13', 'macbook 4gb'
-				'macbook 500gb', 'lenovo', 'yoga', 'surface', 'convertible surface', 'convertible yoga', 'microsoft']
+				'macbook 500gb', 'lenovo yoga', 'yoga', 'surface', 'convertible surface', 'convertible yoga', 'microsoft surface']
 
-	wearables = ['watch', 'watch 38', 'watch 42', 'watch 42mm', 'watch 38mm', 'apple watch', 'watch apple', 'suunto', 'watch ambit', 'ambit', 'watch v800', 
-				'v800', 'polar', 'asus', 'watch asus', 'asus watch']
+	wearables = ['watch', 'watch 38', 'watch 42', 'watch 42mm', 'watch 38mm', 'apple watch', 'watch apple', 'suunto watch', 'watch ambit', 'ambit', 'watch v800', 
+				'v800', 'polar watch', 'watch asus', 'asus watch']
 
 	smart = ['amazon alexa', 'alexa', 'alexa dot', 'alexa echo', 'tchibo', 'tchibo qbo', 'qbo', 'qbo milk', 'samsung robot', 'robot vacuum', 'vacuum', 
 			'vacuum cleaner', 'powerbot', 'VR20J9020UR', 'VR20J9259U']
 
+	brands = ['apple', 'samsung', 'parrot', 'htc', 'oculus', 'microsoft', 'lenovo', 'suunto', 'polar', 'asus', 'amazon', 'tchibo']
+
 	ENTITIES = { 'phones':phones, 'drones':drones, 'gaming':gaming, 'computing':computing, 'wearables':wearables, 'smart':smart}
 
 	
+
+
 	all_entities = phones + drones + gaming + computing + wearables + smart
 
 	show_price = ['show me price', 'how much', 'cost of', 'tell me the price', 'give me the price', 'how much money']
 	show_products = ['show me', 'I want a', 'I need a', 'I wish a', 'I am looking for a', 'I look for a', 'show me a', 'I am interested','tell me about']
+	show_products_brand = ['show', 'looking for', 'do you have', 'products']
+
 
 	data_show_price = []
 	for ent in all_entities:
@@ -95,40 +102,23 @@ def more_data():
 	data_show_similar_products = data_show_similar_products + all_entities
 	labels_1 = [1] * len(data_show_similar_products) 
 
-	return data_show_price + data_show_similar_products, labels_0 + labels_1
+	data_brands = []
+	for b in brands:
+		for s in show_products_brand:
+			data_brands.append(s + " " + b)
+	#Append all brands 
+	data_brands = data_brands + brands
+	labels_2 = [2] * len(data_brands)
+			
+	return data_show_price + data_show_similar_products + data_brands, labels_0 + labels_1 + labels_2
 
-
-
-def create_data():
-	'''
-	This is an example to create some data in order to train the machine learning model
-	'''
-	WORDS_DESIRE = ['I want a', 'I need a', 'I wish a', 'I am looking for a', 'I look for a', 'show me a', 'I am interested']
-	samsung = ['samsung', 'galaxy', 'smartphone', 'samsung galaxy', 'galaxy s8', 'phone']
-	iphone = ['iphone', 'iphone 7', 'plus', 'apple iphone']
-	phones = samsung + iphone
-	drone = ['drone', 'parrot', 'plane']
-	gaming = ['vive', 'htc', 'virtual reality', 'virtual glasses', 'vr', 'oculus', 'oculus rift']
-	computing = ['apple macbook', 'apple macbook air', 'mac', 'laptop', 'computer']
-	wearables = ['apple watch', 'smartwatch' , 'watch', 'ambit', 'suunto', 'polar', 'v800']
-	smart_home = ['alexa dot', 'alexa echo', 'amazon', 'qbo milk master' , 'tchibo', 'robotic vacuum cleaner']
-	all_cats = [phones, drone, gaming, computing, wearables, smart_home]
-
-	sentences = []
-	labels = []
-
-	for idx, cat in enumerate(all_cats):
-		for product in cat:
-			for desire in WORDS_DESIRE:
-				sentences.append(desire + " " + product)
-		labels = labels + [idx] * (len(cat) * len(WORDS_DESIRE))
-	return sentences, labels 
 
 def find_intent(clf, input_sentence, transformer):
 	'''
-	Funtion that maps a input sentence from the user to the 
-	desire category from {Phones: 0, Drones: 1, Gaming:2, Computing: 3, Wearables:4 , Smarthomes:5}
-	It returns the category number 
+	Funtion that maps a input sentence from the user to a intent 
+	0: Search Price of products 
+	1: Search similar products
+	2: Search products by brand 
 	'''
 	#Preprocess the input string
 	clean_sentence = preprocess_senteces(input_sentence)
@@ -139,39 +129,12 @@ def find_intent(clf, input_sentence, transformer):
 	#If the prediction is not so sure from the user input return -1
 	proba = clf.predict_proba(features)[0]
 
-	print(proba)
 	proba = np.array(proba)
 
-	if (proba > 0.9).sum() > 0:
+	if (proba > 0.7).sum() > 0:
 		return clf.predict(features)[0]
 
 	return -1
-
-
-
-def find_category(clf, input_sentence, transformer):
-	'''
-	Funtion that maps a input sentence from the user to the 
-	desire category from {Phones: 0, Drones: 1, Gaming:2, Computing: 3, Wearables:4 , Smarthomes:5}
-	It returns the category number 
-	'''
-	#Preprocess the input string
-	clean_sentence = preprocess_senteces(input_sentence)
-
-	#Create the features with the transformer
-	features = transformer.transform([clean_sentence])
-
-	#If the prediction is not so sure from the user input return -1
-	proba = clf.predict_proba(features)[0]
-
-	print(proba)
-	proba = np.array(proba)
-
-	if (proba > 0.9).sum() > 0:
-		return clf.predict(features)[0]
-
-	return -1
-
 
 def find_entities(sentence):
 	entities = []
@@ -183,63 +146,46 @@ def find_entities(sentence):
 	# do the nlp stuff
 	tokenized = nltk.word_tokenize(clean_s)
 	nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)] 
-
 	return nouns
 
 
-
-
 def do_intent(intent, entities, df):
-	if intent == 1: ##Show products similar to entities
-		print("opp")
-
-
-
-
-
-
-def find_category(clf, input_sentence, transformer):
 	'''
-	Funtion that maps a input sentence from the user to the 
-	desire category from {Phones: 0, Drones: 1, Gaming:2, Computing: 3, Wearables:4 , Smarthomes:5}
-	It returns the category number 
+	Execute the different intents:
+	0: Search Price of products 
+	1: Search similar products
+	2: Search products by brand 
 	'''
-	#Preprocess the input string
-	clean_sentence = preprocess_senteces(input_sentence)
+	#Make sure entities is no empty
+	if not entities:
+		#print("Bot: I am sorry but at the moment we don't have what you are looking for :/...")
+		return
 
-	#Create the features with the transformer
-	features = transformer.transform([clean_sentence])
+	if intent == 1: 
+		products = df.loc[df['Product Name'].str.contains('|'.join(map(re.escape, entities)), case=False)]	
+		if products.empty:
+			return
 
-	#If the prediction is not so sure from the user input return -1
-	proba = clf.predict_proba(features)[0]
-	#print(proba)
-	proba = np.array(proba)
+		print("Bot: We have the following products you might be interested:")
+		for prod in products['Product Name'].values:
+			print("Bot: " + prod)
+		return products
 
-	if (proba > 0.5).sum() > 0:
-		return clf.predict(features)[0]
+	elif intent == 0:
+		products = df.loc[df['Product Name'].str.contains('|'.join(map(re.escape, entities)), case=False)]
+		print("Bot: We have the following prices for the products searched:")
+		for prod,price  in products[['Product Name', 'Subscription Plan']].values:
+			print("Bot: The montly price for " + prod + " is: " + str(price) + " .")
 
-	return -1
+	elif intent == 2:
+		products = df.loc[df['Brand'].str.contains('|'.join(map(re.escape, entities)), case=False)]	
+		if products.empty:
+			return
 
-
-def display_brands(cat, df):
-	'''
-		CAT = {Phones: 0, Drones: 1, Gaming:2, Computing: 3, Wearables:4 , Smarthomes:5}
-	'''
-	#Get products from category and display
-	cats = {0:'Phones & Tablets', 1:'Drones', 2:'Gaming & VR', 3:'Computing', 4:'Wearables', 5:'Smart Home'}
-
-	brands = df['Brand'].loc[df['Category'] == cats[cat]].values
-
-	brands = set(brands)
-
-	print("I see, you are interested in " + cats[cat] + "!")
-	print("We currently have the following brands: ")
-
-	for idx, brand in enumerate(brands):
-		print(brand)
-	
-	return brands
-
+		print("Bot: We have the following products from " + entities[0])
+		for prod in products['Product Name'].values:
+			print("Bot: " + prod)
+		return products
 
 def is_a_brand(brand_list, brand):
 	for b in brand_list:
@@ -247,36 +193,22 @@ def is_a_brand(brand_list, brand):
 			return True 
 	return False
 
+def search_price_print(product, df):
+	'''
+		Df is a dataframe with only products similar to the users choice
+	'''
+	one_product = df.loc[df['Product Name'].str.contains(product, case=False, regex=False)]
 
-def display_products(brand, df, cat):
+	if one_product.empty:
+		print("Bot: Sorry, but this product is not available...")
 
-	cats = {0:'Phones & Tablets', 1:'Drones', 2:'Gaming & VR', 3:'Computing', 4:'Wearables', 5:'Smart Home'}
-
-	all_brands = df['Brand'].values
-	products = []
-	for b in all_brands:
-		if brand == b.lower():
-			mask = (df['Brand'] == b ) & (df['Category'] == cats[cat])
-			products = df['Product Name'].loc[ mask ].values
-			print("Here are all products from " + b + "!")
-			break
-
-	for idx, product in enumerate(products):
-		print( str(idx) + ": " + product)
-
-	return products
-
+	else:
+		for name, price in one_product[['Product Name', 'Subscription Plan']].values:
+			print("Bot: The montly price for " + name + " is: " + str(price) + " .")
 
 	
-def display_subscription(index, products, df):
-	if index >= len(products):
-		print("Incorrect index")
- 
-	else:
-		price = df['Subscription Plan'].loc[df['Product Name'] == products[index]].values[0]
-		print("The price for " + products[index] + " is: " + str(price) + " for a month.")
-
-
+def print_error_msg():
+	print("Bot: I am affraid we dont have what are you looking for..:/, please try again.")
 
 
 if __name__ == '__main__':
@@ -284,21 +216,16 @@ if __name__ == '__main__':
 	
 	#Load dataframe 
 	df = pd.read_csv('data.csv', index_col=0)
-	print(df.head())
-
+	#print(df.head())
 
 	#Create some data 
 	#sentences, labels = create_data()
 	sentences, labels = more_data()
 
-
-	clean_sentences = []
-
 	#Clean the training data 
+	clean_sentences = []
 	for s in sentences:
 		clean_sentences.append(preprocess_senteces(s))
-
-	#print(clean_sentences)
 
 	#Split train test 
 	X_train, X_test, y_train, y_test = train_test_split(clean_sentences, labels, test_size=0.2, random_state=42)
@@ -310,7 +237,7 @@ if __name__ == '__main__':
 	#Bag of words model
 	train_data_features = vectorizer.fit_transform(clean_sentences)
 	train_data_features = train_data_features.toarray()
-	print(train_data_features.shape)
+	#print(train_data_features.shape)
 
 	#Bag of TF-IDF model
 	train_tfidf = vectorizer_tfidf.fit_transform(clean_sentences)
@@ -328,153 +255,59 @@ if __name__ == '__main__':
 	neigh = KNeighborsClassifier(n_neighbors=3, algorithm='brute')
 	neigh.fit(train_data_features, labels)
 
-
 	#Some test sentences
-	test_sentences = ['I want to know more about an iphone', 'I am interested in a s8', 'show me price for alexa echo', 'I look for shoes', 'apple iphone']
+	test_sentences = ['I want to know more about an iphone', 'I am interested in a s8', 'show me price for alexa echo', 'I look for shoes', 'apple iphone', 'show samsung', 'samsung']
 
 	clean_test = [preprocess_senteces(x) for x in test_sentences]
 	test_features = vectorizer.transform(clean_test)
 	test_tfidf = vectorizer_tfidf.transform(clean_test)
 
-	print(forest.predict(test_features))
+	#print(forest.predict(test_features))
 
 	#Visualize some training data using TSNE
 	visualize = TSNE(n_components=2, random_state=42)
 	visualize_data = visualize.fit_transform(train_tfidf)
-
-	print(visualize_data.shape)
 	x_v = visualize_data[:,0]
 	y_v = visualize_data[:,1]
 	plt.scatter(x_v, y_v, c=labels)
 	#plt.show()
 
-
-
-
 	requests = []
-
-
 	cats = pd.unique(df['Category'])
 	no_cats = len(cats)
 	INITIAL_GREETING = "Bot: Welcome at Grover!!, I am your personal assistant"
 
 	print(INITIAL_GREETING)
-
+	#Main loop 
 	try: 
 		while True:
+			print("")
 			print('Bot: What are you looking for?')
 			user_input = input('User: ')
 			intent = find_intent(forest, user_input, vectorizer)
 			entities = find_entities(user_input)
-
-
 			requests.append(request(user_input, intent, entities)) 
 
 			if intent < 0:
-				print("Bot: I am affraid we dont have what are you looking for..:/, please try again.")
+				print_error_msg()
+
+			elif intent == 0:
+				do_intent(intent, entities, df)
 			
-			do_intent(intent, entities, df)
+			elif intent >= 1:
+				products = do_intent(intent, entities, df)
+				if products is None:
+					print_error_msg()
 
-
-			
-
-	except KeyboardInterrupt:
-		print('Have a nice day!')
-
-	'''
-
-	try: 
-		while True:
-			print('Bot: What are you looking for?')
-			user_input = input('User: ')
-			cat = find_category(forest, user_input, vectorizer)
-			
-			if cat < 0:
-				print("Bot: I am affraid we dont have what are you looking for..:/, please try again.")
-
-			else:#Print the brands from the data
-
-				brands_interest = display_brands(cat, df)
-
-				#Ask for the brand of interest
-				print("Bot: From which brand you want to see products?")
-				brand = preprocess_senteces(input("User: "))
-				if is_a_brand(brands_interest, brand):
-					#Show products from the brand
-					products = display_products(brand, df, cat)
-
-					product_user = input("Bot: Please type the id of the product :).")
-
-					#Display the plab subscription
-					display_subscription(int(product_user), products, df)
-
-
-	except KeyboardInterrupt:
-		print('Have a nice day!')
-
-	'''
-
-
-
-
-	#Create test features
-	#test_data_features = vectorizer.fit_transform(clean_sentences)
-	#test_data_features = test_data_features.toarray()
-
-	#With tfidf features
-	#test_tfidf = vectorizer_tfidf.fit_transform(clean_sentences)
-
-
-	print(forest.predict(test_features))
-	print(forest2.predict(test_tfidf))
-	print(neigh.predict(test_features))
-
-	#Print the score for the two Random forest, each trained with different features
+				else:
+					print("Bot: Are you interested in any of the products y/n?")
+					res = input("User: ")
+					if 'y' in res:
+						print("Bot: Which characteristics?")
+						res = input("User: ")
+						price = search_price_print(res, products)
 	
-	#print(forest.score(train_data_features, labels))
-	#print(forest2.score(train_data_features , labels))
+	except KeyboardInterrupt:
+		print('Bot: Have a nice day!')
 
-
-
-
-
-
-
-'''
-Easy harcoded implementation 
-
-try: 
-	while True:
-		#Print categories available
-		print("What categories are you interested in (type the id):")
-		for j in range(len(cats)):
-			print(str(j) + ": " + cats[j])
-	
-		user_category = int(input())
-
-		if user_category >= len(cats):
-			print("This option doesnt exist")
-
-		#Get products from category and display
-		products = df['Product Name'].loc[df['Category'] == cats[user_category]].values
-
-		print("Products available from " + cats[user_category] + " found:")
-		for i in range(len(products)):
-			print(str(i) + ": " + products[i])
-
-		#Get the interested product:
-		id_product = int(input())
-
-		#Display the plan subscription
-		plans = df['Subscription Plan'].loc[df['Product Name'] == products[id_product]].values
-		price_plan =  str(df['Subscription Plan'].loc[df['Product Name'] == products[id_product]].values[0])
-		print("Price subscription: " + price_plan) 
-		if input("Do you want to keep searching for more products y/n? \n") == 'n':
-			print("Have a nice day!")
-			break
-
-except KeyboardInterrupt:
-	print('Interrupted!')
-
-'''
 	
